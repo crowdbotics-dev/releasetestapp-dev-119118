dcd4b4b34083603a33094a3d14da69b35d0847da
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-09-29 22:41:51 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Ravinderyadavpath/index.js
+++ screens/Ravinderyadavpath/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
-const Ravinderyadavpath = () => {
+const Ravinderyadavpath = ({
+  route
+}) => {
+  const {} = route.params || {};
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
     }}></ScrollView>
     </SafeAreaView>;
@@ -14,7 +17,8 @@ const Ravinderyadavpath = () => {
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%",
+    backgroundColor: "#663535"
   }
 });
 export default Ravinderyadavpath;
